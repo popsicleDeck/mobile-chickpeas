3f55d60e9c89705be1995567b00248b6b9f10380
popsicleDeck <nguyenchuminh125@gmail.com>
2025-04-19 19:35:03 +1000

    add mappings + options

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
 require("config.lazy")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+
+vim.keymap.set('n', '<Leader><Leader>', '<Cmd>so<Space>%<Tab><CR>', { desc = 'Source current file' })
+
+
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,7 +38,11 @@ require("lazy").setup({
   lazy = false,
   priority = 1000,
   config = function()
-	  vim.cmd([[colorscheme tokyonight]])
+	  vim.cmd([[colorscheme tokyonight-night]])
+	  vim.cmd.highlight({ "Comment", "guifg=#6c77ab" })
+
+	  
+
   end,
   },
 })
