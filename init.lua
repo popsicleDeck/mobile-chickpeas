efe132458657054603168f0716ffe46459b2b682
popsicleDeck <nguyenchuminh125@gmail.com>
2025-06-01 16:55:50 +1000

    add bufnr in statusline

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ require("lazy").setup({
 require("config.autocmd")
 
 vim.g.python3_host_prog = "/usr/bin/python3"
-
 vim.g.have_nerd_font = true
 
 vim.opt.number = true
@@ -18,6 +17,7 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.cursorline = true
 -- Minimal number of lines above and below the cursor
 vim.opt.scrolloff = 7
+vim.opt.statusline = "%n %<%f %h%m%=%-14.(%l,%V%) %P" --add bufnr, remove a few things that aren't needed
 
 vim.keymap.set("n", "<Leader><Leader>", "<Cmd>so<Space>%<Tab><CR>", { desc = "Source current file" })
 vim.keymap.set("n", "<Leader>o", "<Cmd>Oil<CR>", { desc = "Run Oil" })
