3ce8158ad61736d05cbf649319bb5ce2a0a4ca4e
popsicleDeck <nguyenchuminh125@gmail.com>
2025-06-02 22:10:00 +1000

    fix the autocmd to printout ouput by adding an usercommand

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,29 +1,30 @@
 vim.api.nvim_create_augroup("myAutocmd", { clear = true })
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = "myAutocmd",
-	pattern = "*.py",
-	callback = function()
-		local bufnr = tonumber(vim.fn.input("Bufnr to print output: "))
-		if not vim.api.nvim_buf_is_valid(bufnr) then
-			vim.notify("Bufnr not exists")
-			bufnr = tonumber(vim.fn.input("Please re-enter: "))
-		end
-		local fname = vim.fn.input("Filename: ")
-		vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "Output:" })
-		local append_data = function(_, data)
-			if data then
-				vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+local attach_to_buffer = function(bufnr, pattern, command)
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = "myAutocmd",
+		pattern = pattern,
+		callback = function()
+			vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "Output:" })
+			local append_data = function(_, data)
+				if data then
+					vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+				end
 			end
-		end
 
-		vim.fn.jobstart({ "python3", fname }, {
-			stdout_buffered = true,
-			on_stdout = append_data,
-			on_stderr = append_data,
-		})
-	end,
-})
+			vim.fn.jobstart(command, {
+				stdout_buffered = true,
+				on_stdout = append_data,
+				on_stderr = append_data,
+			})
+		end,
+	})
+end
+vim.api.nvim_create_user_command("Run", function()
+	local bufnr = (tonumber(vim.fn.input("Bufnr: ")))
+	local pattern = vim.fn.input("Pattern: ")
+	local command = vim.split(vim.fn.input("Command and file name: "), " ")
+	attach_to_buffer(bufnr, pattern, command)
+end, {})
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = "myAutocmd",
